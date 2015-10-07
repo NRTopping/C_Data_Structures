48b97a6b6f01ad0e7a22fc89da5fa196e0e849ff
NRTopping <NRTopping@email.arizona.edu>
2015-10-06 22:49:38 -0700

    start outlining slist (old linkedlist, simplified)

---
+++ include/NDS/general.h
@@ -0,0 +1,10 @@
+#ifndef NDS_GENERAL_H_
+#define NDS_GENERAL_H_
+
+typedef void* nds_element_t; 
+
+typedef nds_element_t(* nds_alloc_func_t)(void *data);
+typedef void(* nds_free_func_t)(nds_element_t e);
+typedef nds_element_t(* nds_copy_func_t)(const nds_element_t e);
+typedef int(* nds_compar_func_t)(const nds_element_t e, void *value);
+#endif /* NDS_GENERAL_H_ */
--- slist/slist.h
+++
@@ -1,9 +0,0 @@
-#ifndef SLIST_H_
-#define SLIST_H_
-
-struct slist_t {
-
-};
-
-
-#endif /* SLIST_H_ */
