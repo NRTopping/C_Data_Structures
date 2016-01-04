9d8626f87c6a23bb48b8cda513137744348025b1
NRTopping <NRTopping@email.arizona.edu>
2016-01-03 22:25:54 -0800

    added remove and delete function bodies, minus general remove logic

--- include/NDS/list.h
+++ include/NDS/list.h
@@ -128,12 +128,89 @@ extern void nds_list_insert_head(const nds_list_t list, const nds_element_t data
  * @return void
  */
 extern void nds_list_insert_tail(const nds_list_t list, const nds_element_t data); 
+
+/**
+ * @brief Remove Element from Beginning of List
+ *
+ * This function removes the element from the beginning of the list, without
+ * freeing the data. Will return null if no data is currently stored in the list
+ *
+ * @pre list should be initialized and non-null
+ * @param list the list to remove data from 
+ * @return pointer to element in list that was removed
+ */
 extern nds_element_t nds_list_remove_head(const nds_list_t list); 
+
+/**
+ * @brief Remove Element from End of List
+ *
+ * This function removes the element from the end of the list, without freeing
+ * the data itself. Will return null if no data is currently stored in the list
+ *
+ * @pre list should be initialized and non-null
+ * @param list the list to remove dat from 
+ * @return pointer to element in list that was removed
+ */
 extern nds_element_t nds_list_remove_tail(const nds_list_t list); 
+
+/**
+ * @brief Remove Specific Element From List
+ *
+ * This function removes the first element of a specific value from a list,
+ * without freeing the data itself. Will return null if no data is currently
+ * stored in the list, or if the value was not found.
+ *
+ * @pre list should be initialized and non-null. compar_func should be non-null
+ * @param list the list to search through
+ * @param compar_func function used to compare elements in the list to eachother
+ * @param element the element to remove from the list. Can be null
+ * @return the element that was removed from the list, or NULL if no value
+ * found.
+ */
 extern nds_element_t nds_list_remove(const nds_list_t list, 
     const nds_compar_func_t compar_func, const nds_element_t element);
+
+/**
+ * @brief Delete First Element in List
+ *
+ * This function removes the first element from the list, and frees the data
+ * allocated to the element using the free_func stored in the list. 
+ *
+ * @pre list should be non-null and initialized. list should have been
+ * initialized with a free_func that corresponds to the data stored in the list
+ * @param list the list to delete the head element from 
+ * @return void
+ */
 extern void nds_list_delete_head(const nds_list_t list); 
+
+/**
+ * @brief Delete Last Element in List
+ *
+ * This function removes the last element from the list, and frees the data
+ * allocated to the element using the free_func stored in the list. 
+ *
+ * @pre list should be non-null and initialized. list should have been
+ * initialized with a free_func that corresponds to the data stored in the list
+ * @param list the list to delete the head element from 
+ * @return void
+ */
 extern void nds_list_delete_tail(const nds_list_t list); 
+
+/**
+ * @brief Deletes Element in List
+ *
+ * This function deletes the first instance of an element in the given list, and
+ * frees the data allocated to the element using the free_func store in the list.
+ * 
+ * @pre list should be non-null and initialized. list should have been
+ * initialized with a free_func that corresponds to the data stored in the list
+ * @param list the list to delete the element from
+ * @param compar_func comparison function used to compare elements in the list
+ * to eachother
+ * @param element the element to find in the list, can be null if compar_func
+ * uses other means to find element.
+ * @return void
+ */
 extern void nds_list_delete(const nds_list_t list, 
     const nds_compar_func_t compar_func, const nds_element_t element); 
 extern nds_element_t nds_list_search(const nds_list_t list, 
--- src/NDS/list.c
+++ src/NDS/list.c
@@ -88,8 +88,8 @@ extern nds_element_t nds_list_get_tail(const nds_list_t list) {
 extern void nds_list_insert_head(const nds_list_t list, 
     const nds_element_t data) { 
   assert(list != NULL); 
-  nds_lnode_t next = nds_lnode_get_next(list->beg), 
-              newN = nds_lnode_alloc(); 
+  const nds_lnode_t next = nds_lnode_get_next(list->beg), 
+                    newN = nds_lnode_alloc(); 
 
   nds_lnode_set_data(newN, data);
   nds_lnode_link(list->beg, newN);
@@ -99,9 +99,67 @@ extern void nds_list_insert_head(const nds_list_t list,
 extern void nds_list_insert_tail(const nds_list_t list,
     const nds_element_t data) { 
   assert(list != NULL);
-  nds_lnode_t prev = nds_lnode_get_prev(list->end),
-              newN = nds_lnode_alloc();
+  const nds_lnode_t prev = nds_lnode_get_prev(list->end),
+                    newN = nds_lnode_alloc();
   nds_lnode_set_data(newN, data); 
   nds_lnode_link(prev, newN);
   nds_lnode_link(newN, list->end);
+}
+
+extern nds_element_t nds_list_remove_head(const nds_list_t list) { 
+  assert(list != NULL);
+  const nds_lnode_t head    = nds_lnode_get_next(list->beg), 
+                    newHead = nds_lnode_get_next(head);
+  const nds_element_t ret = nds_lnode_get_data(head);
+ 
+  // Don't modify list if head is the end sentinel of the list.
+  if (head == list->end) return NULL;
+
+  nds_lnode_link(list->beg, newHead); 
+  nds_lnode_free(head);
+
+  return ret;
+}
+
+extern nds_element_t nds_list_remove_tail(const nds_list_t list) { 
+  assert(list != NULL); 
+  const nds_lnode_t tail    = nds_lnode_get_prev(list->end),
+                    newTail = nds_lnode_get_prev(tail);
+  const nds_element_t ret = nds_lnode_get_data(tail);  
+ 
+  if (tail == list->beg) return NULL;
+
+  nds_lnode_link(newTail, list->end);
+  nds_lnode_free(tail);
+
+  return ret;
+}
+
+extern nds_element_t nds_list_remove(const nds_list_t list, 
+    const nds_compar_func_t compar_func, const nds_element_t element) { 
+  // TODO
+  compar_func(list->beg, element);
+  return NULL;
+}
+
+extern void nds_list_delete_head(const nds_list_t list) { 
+  assert(list != NULL && list->free_func != NULL);
+  const nds_element_t headVal = nds_list_remove_head(list);
+
+  if (headVal != NULL) list->free_func(headVal);
+}
+
+extern void nds_list_delete_tail(const nds_list_t list) { 
+  assert(list != NULL && list->free_func != NULL);
+  const nds_element_t tailVal = nds_list_remove_tail(list);
+
+  if (tailVal != NULL) list->free_func(tailVal);
+}
+
+extern void nds_list_delete(const nds_list_t list, 
+    const nds_compar_func_t compar_func, const nds_element_t element) { 
+  assert(list != NULL && list->free_func != NULL);
+  const nds_element_t val = nds_list_remove(list, compar_func, element); 
+
+  if (val != NULL) list->free_func(val);
 }
